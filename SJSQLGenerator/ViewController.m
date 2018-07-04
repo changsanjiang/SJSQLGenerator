878ddc47e3e43ec854593dc2ab3d5064bdec8953
changsanjiang <changsanjiang@gmail.com>
2018-07-04 20:41:44 +0800

    Test

--- SJSQLGenerator/ViewController.m
+++ SJSQLGenerator/ViewController.m
@@ -20,43 +20,49 @@
     [super viewDidLoad];
     
     
-    /// 选出id等于10的商品
-    SELECT(s(Product)).FROM("Product").WHERE(c1(Product, prod_id = 10));
+    SELECT(s(Product)).FROM("Products").WHERE(c1(Product, prod_id = 10));
+    SELECT(s1(Product, prod_name)).FROM("Products").WHERE(c1(Product, prod_id = 10));
+    SELECT(s2(Product, prod_name, prod_price)).FROM("Products").WHERE(c1(Product, prod_id = 10));
+    SELECT(s3(Product, prod_name, prod_price, prod_desc)).FROM("Products").WHERE(c1(Product, prod_id = 10));
+    SELECT(s4(Product, prod_name, prod_price, prod_desc, vend_id)).FROM("Products").WHERE(c1(Product, prod_id = 10));
     
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 
+/// 测试SELECT
 - (void)testSelect {
-    
     s(Product);
     s1(Product, prod_id);
     s2(Product, prod_id, prod_name);
     s3(Product, prod_id, prod_name, prod_price);
     s4(Product, prod_id, prod_name, prod_price, prod_desc);
     s5(Product, prod_id, prod_name, prod_price, prod_desc, vend_id);
-    
 }
 
+/// 测试查询条件
 - (void)testCondition {
     
+    /// 产品id等于10的产品
     /// 等于
-    /// 产品id等于10的商品
     c1(Product, prod_id = 10);
     
+    /// 产品id小于20, 大于10的产品
     /// AND
-    /// 产品id小于20, 大于10的商品
     c2(Product, prod_id <= 20, prod_id >= 10);
-    
+
+    /// 产品名称等于`apple`或者等于`banana`的产品
     /// OR
-    c2(Product, prod_name = @"bear", prod_name = @"hear");
+    c2(Product, prod_name = @"apple", prod_name = @"banana");
     
+    /// 产品名称等于`apple`或者等于`banana`, 并且供应商的ID等于2008的产品
     /// (OR) + AND
+    c3(Product, prod_name = @"apple", prod_name = @"banana", vend_id = 2008);
     
-    
+    /// 产品ID等于`10`, `11`, `12`, `13`的产品
     /// IN
-    
-    
+    id ids = @[@(10), @(11), @(12), @(13)];
+    c1(Product, prod_id = ids); // 报警告, 想办法处理一下
 }
 
 @end
